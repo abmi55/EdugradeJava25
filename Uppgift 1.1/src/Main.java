e192d2627d32d0f613081c3fd7f58eb8f795d6f9
Abbas Mirhazar <abbas.mirhazar.java25@edu.edugrade.se>
2025-09-10 19:23:05 +0200

    Uppgift 1.1

---
+++ Uppgift 1.1/src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 30;  //Heltal
+        double height = 1.75;  //Decimal tal
+        char initial = 'A';  //Ett täcken
+        String name = "ALice"; //En text
+        boolean isStudent = true;  //Sant/falskt värde
+
+        //Nu skriver vi ut variablerna
+        System.out.println("Ålder:" + age);
+        System.out.println("Längd:" + height);
+        System.out.println("initial:" + initial);
+        System.out.println("Namn:" + name);
+        System.out.println("isStudent:" + isStudent);
+
+    }
+}
