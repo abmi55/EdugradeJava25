aa9cebe0e1d91916ddb842a3e4afa222ad85f79d
Abbas Mirhazar <abbas.mirhazar.java25@edu.edugrade.se>
2025-09-10 20:04:42 +0200

    Uppgift 2.1

---
+++ Uppgift 2.1/src/Main.java
@@ -0,0 +1,18 @@
+public class Main  {
+    public static void main(String[] args) {
+        int sum = 10 + 20;
+        System.out.println("summan av 10 + 20 = " + sum);
+
+        int difference =  100 - 30;
+        System.out.println("skillnaden av 100 - 30 = " + difference);
+
+        int product = 5 * 7;
+        System.out.println("produkten av 5 * 7 = " + product);
+
+        int quotient = 20 / 4;
+        System.out.println("kvoten av 20 / 4 = " + quotient);
+
+        int reminder = 10 % 3;
+        System.out.println("resten av 10 % 3 = " + reminder);
+    }
+}
